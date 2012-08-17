fa6aaeee5ae9fd699f24e6662065ddb312fab55a
Yahya Kamran <yahya.kamran@conradlabs.com>
2012-08-17 11:35:24 +0500

    Adding local maven repo, with metadata-extractor-2.3.1.jar

---
+++ src/main/java/com/rackspace/cloud/api/docs/RackspaceMetadataExtractor.java
@@ -0,0 +1,11 @@
+package com.rackspace.cloud.api.docs;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ykamran
+ * Date: 8/16/12
+ * Time: 5:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RackspaceMetadataExtractor {
+}
